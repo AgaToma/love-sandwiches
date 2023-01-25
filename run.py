d5bb39bea575ac6064b759dc78e7c74577a96b8b
AgaToma <aga.tomashevska@gmail.com>
2023-01-25 18:22:04 +0000

    Add function to capture sales from last 5 markets

--- run.py
+++ run.py
@@ -86,6 +86,20 @@ def calculate_surplus_data(sales_row):
    
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Gets last 5 entries from a column
+    of specific sandwich type
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[5:])
+    
+    return columns
+
 def main():
     """
     Run all program functions
@@ -98,3 +112,5 @@ def main():
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
+
+sales_columns = get_last_5_entries_sales()
